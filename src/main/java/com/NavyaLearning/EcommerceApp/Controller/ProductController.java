da4e85889d78759dd45f07bc3956808c27d4a152
Navya Tewari <navya.tewari@telekom-digital.com>
2026-02-26 23:27:24 +0530

    Image support :)

--- src/main/java/com/NavyaLearning/EcommerceApp/Controller/ProductController.java
+++ src/main/java/com/NavyaLearning/EcommerceApp/Controller/ProductController.java
@@ -3,8 +3,10 @@ package com.NavyaLearning.EcommerceApp.Controller;
 import com.NavyaLearning.EcommerceApp.Model.Product;
 import com.NavyaLearning.EcommerceApp.Service.ProductService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -38,6 +40,31 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        try {
+            Product product1 = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
+        Product product = productService.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+
+        //we have to send http code
+        //the content type so that front end can handle it
+        //and the actual image bytes
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
     }
 
 }
--- src/main/java/com/NavyaLearning/EcommerceApp/Service/ProductService.java
+++ src/main/java/com/NavyaLearning/EcommerceApp/Service/ProductService.java
@@ -3,7 +3,9 @@ package com.NavyaLearning.EcommerceApp.Service;
 import com.NavyaLearning.EcommerceApp.Model.Product;
 import com.NavyaLearning.EcommerceApp.Repo.ProductRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -21,5 +23,12 @@ public class ProductService {
     public Product getProductById(int id)
     {
         return productRepo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepo.save(product);
     }
 }
